1540871f7c1ab09666c8f605a55cfaa8bc78b625
RomeoHeradhiningrat <69361719+RomeoHeradhiningrat@users.noreply.github.com>
2026-09-25 17:46:58 +0700

    Update mover_node.py

--- mover_node.py
+++ mover_node.py
@@ -3,12 +3,13 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 import time
 
+
 class MoverNode(Node):
     def __init__(self):
         super().__init__('mover_node')
-        
+
         self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
-        
+
         self.timer = self.create_timer(0.1, self.timer_callback)
         self.start_time = time.time()
 
@@ -17,62 +18,63 @@ class MoverNode(Node):
         current_time = time.time()
         elapsed_time = current_time - self.start_time
 
-        if elapsed_time < 2.0:
-            msg.linear.x = 0.5   # Kecepatan linear (m/s)
+        if elapsed_time < 5.0:
+            msg.linear.x = 0.5
             msg.angular.z = 0.0
             self.get_logger().info('Gerak Maju Kedepan...')
-			
-		elif elapsed_time < 4.0:
-			msg.linear.x = 0.0
-			msg.angular.z = 0.5 # Kecepatan rotasi (rad/s)
-			self.get_logger().info('Belok...')
-			
-		elif elapsed_time < 9.0:
-            msg.linear.x = 0.5   # Kecepatan linear (m/s)
+
+        elif elapsed_time < 8.14:
+            msg.linear.x = 0.0
+            msg.angular.z = 0.5
+            self.get_logger().info('Belok...')
+
+        elif elapsed_time < 11.14:
+            msg.linear.x = 0.5
             msg.angular.z = 0.0
             self.get_logger().info('Gerak Maju Kekiri...')
-			
-        elif elapsed_time < 11.0:
+
+        elif elapsed_time < 14.28:
             msg.linear.x = 0.0
-            msg.angular.z = 0.5  # Kecepatan rotasi (rad/s)
+            msg.angular.z = 0.5
             self.get_logger().info('Belok...')
-			
-		elif elapsed_time < 13.0:
-            msg.linear.x = 0.5   # Kecepatan linear (m/s)
+
+        elif elapsed_time < 19.28:
+            msg.linear.x = 0.5
             msg.angular.z = 0.0
             self.get_logger().info('Gerak Maju Kebawah...')
-			
-		elif elapsed_time < 18.0:
+
+        elif elapsed_time < 22.42:
             msg.linear.x = 0.0
-            msg.angular.z = 0.5  # Kecepatan rotasi (rad/s)
+            msg.angular.z = 0.5
             self.get_logger().info('Belok...')
-			
-		elif elapsed_time < 20.0:
-            msg.linear.x = 0.5   # Kecepatan linear (m/s)
+
+        elif elapsed_time < 25.42:
+            msg.linear.x = 0.5
             msg.angular.z = 0.0
             self.get_logger().info('Gerak Maju Kekanan...')
-			
-		elif elapsed_time < 22.0:
+
+        elif elapsed_time < 28.56:
             msg.linear.x = 0.0
-            msg.angular.z = 0.5  # Kecepatan rotasi (rad/s)
+            msg.angular.z = 0.5
             self.get_logger().info('Belok...')
-			
+
         else:
             msg.linear.x = 0.0
             msg.angular.z = 0.0
             self.get_logger().info('Berhenti.')
+
             self.publisher_.publish(msg)
-            
             self.timer.cancel()
             rclpy.shutdown()
             return
 
         self.publisher_.publish(msg)
 
+
 def main(args=None):
     rclpy.init(args=args)
     node = MoverNode()
-	
+
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
@@ -81,6 +83,7 @@ def main(args=None):
         if rclpy.ok():
             node.destroy_node()
             rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
